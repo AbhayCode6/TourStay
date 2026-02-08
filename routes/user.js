d95880c29bdd05b98cf3f07e1d3e07c6c108ae54
Abhay Kumar <abhaykumar52866@gmail.com>
2026-02-08 21:31:41 +0530

    performs get and post route

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user.js')
+const wrapAsync = require('../utils/wrapAsync.js')
 
 router.get('/signup',(req,res)=>{
     res.render("./users/signup.ejs")
 })
+
+router.post('/signup',wrapAsync(async(req,res)=>{
+    try{
+    let {email,username,password} = req.body
+    const newUser = User({email,username})
+    const registerUser = await User.register(newUser,password)
+    console.log(registerUser)
+    req.flash('success','Welcome to TourStay!')
+    res.redirect('/listings')
+    }catch(err){
+        req.flash("error",err.message)
+        res.redirect('/signup')
+    }
+    
+}))
 
 module.exports = router
